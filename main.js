0b66db173765107bda90c810ca93b699e165f77e
lucas <lucas.riveill@gmail.com>
2017-12-03 19:07:49 +0100

    added select-map functionality

--- main.js
+++ main.js
@@ -1,5 +1,35 @@
-console.log('Network Map');
+// qunad le DOM est loadé: run
+$(document).ready(function() {
 
-var networkList = document.querySelectorAll('#NetworkList');
+  //JQuery va chercher cet élément
+  var $listItems = $('.NetworkElements')
+              .children('.Menu')
+              .children('li');
 
-console.log(networkList);
+  //loop sur chacun des éléments
+  $listItems.each(function(index, element) {
+    //Binde un event-listener "click"
+    $(element).on('click', function(){
+      // Quand "click", exectue handleClick
+      handleClick(element);
+    });
+  });
+
+});
+
+var handleClick = function(element) {
+  // 1- quel elt a été clické
+  console.log('clicked el', element);
+  // 2- quelle valeur data-map
+  var selectedElement = $(element).data('map');
+  console.log('selectedElement', selectedElement);
+  // 3- aller dans la Map
+  var $Maps = $('.NetworkElements')
+              .children('.Maps');
+  $Maps.find('.Map.is-active').removeClass('is-active');
+  // 4- trouver l'elt qui a le data-map correspondant
+  var img = $Maps.find('[data-map="' + selectedElement + '"]');
+  img.addClass('is-active');
+  // 5- enlever la class "is-active" de l'ancien element
+
+}
