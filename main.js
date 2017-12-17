61ed8629ec9c80bd8cc920437e9a8be7d1f21ab4
lucas <lucas.riveill@gmail.com>
2017-12-17 15:53:29 +0100

    created paris.html / renamed classes after naming convention / styled and made nav responsive

--- main.js
+++ main.js
@@ -3,14 +3,14 @@ $(document).ready(function() {
 
   //JQuery va chercher cet élément
   var $listItems = $('.NetworkElements')
-              .children('.Menu')
+              .children('.NetworkElements-nav')
               .children('li');
 
-  //loop sur chacun des éléments
+  //loop on each element
   $listItems.each(function(index, element) {
-    //Binde un event-listener "click"
+    //Binde with "click" event-listener
     $(element).on('click', function(){
-      // Quand "click", exectue handleClick
+      // when "click"--> execute handleClick
       handleClick(element);
     });
   });
@@ -18,18 +18,16 @@ $(document).ready(function() {
 });
 
 var handleClick = function(element) {
-  // 1- quel elt a été clické
+  // 1- which elt has been clicked on
   console.log('clicked el', element);
-  // 2- quelle valeur data-map
+  // 2- which value for data-map
   var selectedElement = $(element).data('map');
   console.log('selectedElement', selectedElement);
-  // 3- aller dans la Map
-  var $Maps = $('.NetworkElements')
-              .children('.Maps');
-  $Maps.find('.Map.is-active').removeClass('is-active');
-  // 4- trouver l'elt qui a le data-map correspondant
-  var img = $Maps.find('[data-map="' + selectedElement + '"]');
+  // 3-  go to Map
+  var $Medias = $('.NetworkElements').children('.NetworkElements-medias');
+ $Medias.find('.Map.is-active').removeClass('is-active');
+  // 4- find the elt with the correponding data-map
+  var img = $Medias.find('[data-map="' + selectedElement + '"]');
   img.addClass('is-active');
-  // 5- enlever la class "is-active" de l'ancien element
-
+  // 5- take out class "is-active" from former element
 }
